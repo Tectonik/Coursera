dcd44097b939dc0b055a981b932831c1827e48e9
Tectonik <marto7395@gmail.com>
2018-01-30 22:46:38 +0200

    Added second, simpler solution

--- assignment1starterCode/scrooge-coin-solution-2/Transaction.java
+++ assignment1starterCode/scrooge-coin-solution-2/Transaction.java
@@ -1,5 +1,3 @@
-package ScroogeCore;
-
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,7 +8,7 @@ import java.security.PublicKey;
 public class Transaction {
 
     public class Input {
-        /** hash of the ScroogeCore.Transaction whose output is being used */
+        /** hash of the Transaction whose output is being used */
         public byte[] prevTxHash;
         /** used output's index in the previous transaction */
         public int outputIndex;
